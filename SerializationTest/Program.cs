e5a7c2f5415d6cb46fc0ee66e61b7e2acd073350
gooseish <cegooseish@gmail.com>
2025-12-21 18:29:59 -0500

    initial setup

---
+++ SerializationTest/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+Console.WriteLine("Hello, World!");
+Terrain terrain = new Terrain();
+
+string jsonString = JsonSerializer.Serialize(terrain);
+
+Console.WriteLine(jsonString);
+
+
+public class Terrain()
+{
+    public int id;
+    public string name;
+}
+
+[JsonSerializable(typeof(Terrain))]
+[JsonSerializable(typeof(List<Terrain>))]
+public partial class AppJsonSerializerContext : JsonSerializerContext
+{
+}
+
